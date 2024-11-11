00e7bf7ed7832b0d930f9c44adc64c0f8840f6cd
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-11-11 22:34:48 +0100

    fix connection issues

--- web/src/components/Landing.jsx
+++ web/src/components/Landing.jsx
@@ -19,7 +19,7 @@ function Landing({ projectsArray }) {
             <div className="card__wrap">
             {projectsArray.map((project) => {
                 return (
-                    <a href={project.idproject} key={project.idproject} className="detail-link">
+                    <a href={`/projects/${project.idproject}`} key={project.idproject} className="detail-link">
                         <article className="card">
                         <h2 className="card__projectTitle">
                             <span className="card__projectTitle--text">Personal project card</span>
